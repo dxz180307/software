81b1056acb6768895d55186bee338d8904ce37af
gdcp <gdcp@m51.local>
2018-03-07 15:51:10 +0800

    1.第一次修改 2.添加print（”岁月的新欢“）

--- IOSAPP/ViewController.swift
+++ IOSAPP/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("岁月的新欢")
     }
 
     override func didReceiveMemoryWarning() {
